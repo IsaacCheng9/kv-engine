0f313ead31d11c94b96fb4ce7e48de7788d1ac46
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2026-04-06 20:57:26 +0100

    feat: Implement `append()` mthod for `LogFile`

--- src/log_file.cpp
+++ src/log_file.cpp
@@ -1,12 +1,13 @@
 #include "log_file.hpp"
 #include <fcntl.h>
 #include <stdexcept>
+#include <string_view>
 #include <unistd.h>
 
 namespace kv {
 
 LogFile::LogFile(const std::string &path) {
-  fd_ = open(path.c_str(), O_WRONLY | O_CREAT | O_APPEND, 0644);
+  fd_ = open(path.c_str(), O_RDWR | O_CREAT | O_APPEND, 0644);
   // May fail due to invalid path, no permissions, etc.
   if (fd_ == -1) {
     throw std::runtime_error("Failed to open file: " + path);
@@ -14,5 +15,20 @@ LogFile::LogFile(const std::string &path) {
 }
 
 LogFile::~LogFile() { close(fd_); }
+
+void LogFile::append(std::string_view data) {
+  uint32_t length_prefix = data.size();
+  auto length_success = write(fd_, &length_prefix, sizeof(length_prefix));
+  if (length_success == -1) {
+    throw std::runtime_error("Failed to write length prefix");
+  }
+
+  auto data_success = write(fd_, data.data(), data.size());
+  if (data_success == -1) {
+    throw std::runtime_error("Failed to write data");
+  }
+
+  fsync(fd_);
+}
 
 } // namespace kv
