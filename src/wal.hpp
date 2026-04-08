89d93272e348e0dea8231081432551c7c98ba236
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2026-04-08 23:45:08 +0100

    feat: Add header for `replay()` for `WAL`

--- src/wal.hpp
+++ src/wal.hpp
@@ -1,6 +1,7 @@
 #ifndef KV_ENGINE_WAL_HPP
 #define KV_ENGINE_WAL_HPP
 
+#include "memtable.hpp"
 #include <string>
 #include <string_view>
 
@@ -13,6 +14,7 @@ public:
 
   void log_put(std::string_view key, std::string_view value);
   void log_remove(std::string_view key);
+  void replay(Memtable &memtable);
 
 private:
   int fd_;
