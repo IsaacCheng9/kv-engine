7c6bb373bc1eca22d61a3162968c92564e36e46d
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2026-05-03 01:04:30 +0100

    feat: Implement gRPC `Put()`

--- src/grpc_server.cpp
+++ src/grpc_server.cpp
@@ -3,16 +3,35 @@
 namespace kv {
 
 namespace {
-constexpr std::size_t max_key_size = 64 * 1024; // 64 KiB
-// constexpr std::size_t max_value_size = 1024 * 1024; // 1 MiB
+constexpr std::size_t max_key_size = 64 * 1024;     // 64 KiB
+constexpr std::size_t max_value_size = 1024 * 1024; // 1 MiB
 } // namespace
 
 KvStoreServiceImpl::KvStoreServiceImpl(Engine *engine) : engine_(engine) {}
 
 grpc::Status KvStoreServiceImpl::Put(grpc::ServerContext *,
-                                     const kv::v1::PutRequest *,
+                                     const kv::v1::PutRequest *request,
                                      kv::v1::PutResponse *) {
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "Not yet implemented");
+  const std::string &key = request->key();
+  if (key.empty()) {
+    return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                        "Key cannot be empty");
+  }
+  if (key.size() > max_key_size) {
+    return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                        "Key size exceeds maximum allowed size of " +
+                            std::to_string(max_key_size) + " bytes");
+  }
+
+  const std::string &value = request->value();
+  if (value.size() > max_value_size) {
+    return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                        "Value size exceeds maximum allowed size of " +
+                            std::to_string(max_value_size) + " bytes");
+  }
+
+  engine_->put(key, value);
+  return grpc::Status::OK;
 }
 
 grpc::Status KvStoreServiceImpl::Get(grpc::ServerContext *,
