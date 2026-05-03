2cf8fddad42fb66962ddaa30456d89f91e1cca9d
Isaac Cheng <47993930+IsaacCheng9@users.noreply.github.com>
2026-05-03 01:01:20 +0100

    feat: Implement gRPC `Get()`

--- src/grpc_server.cpp
+++ src/grpc_server.cpp
@@ -2,7 +2,11 @@
 
 namespace kv {
 
-// TODO: implement KvStoreServiceImpl methods here.
+namespace {
+constexpr std::size_t max_key_size = 64 * 1024; // 64 KiB
+// constexpr std::size_t max_value_size = 1024 * 1024; // 1 MiB
+} // namespace
+
 KvStoreServiceImpl::KvStoreServiceImpl(Engine *engine) : engine_(engine) {}
 
 grpc::Status KvStoreServiceImpl::Put(grpc::ServerContext *,
@@ -12,9 +16,26 @@ grpc::Status KvStoreServiceImpl::Put(grpc::ServerContext *,
 }
 
 grpc::Status KvStoreServiceImpl::Get(grpc::ServerContext *,
-                                     const kv::v1::GetRequest *,
-                                     kv::v1::GetResponse *) {
-  return grpc::Status(grpc::StatusCode::UNIMPLEMENTED, "Not yet implemented");
+                                     const kv::v1::GetRequest *request,
+                                     kv::v1::GetResponse *response) {
+  const std::string &key = request->key();
+  if (key.empty()) {
+    return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                        "Key cannot be empty");
+  }
+  if (key.size() > max_key_size) {
+    return grpc::Status(grpc::StatusCode::INVALID_ARGUMENT,
+                        "Key size exceeds maximum allowed size of " +
+                            std::to_string(max_key_size) + " bytes");
+  }
+
+  auto value = engine_->get(key);
+  if (!value) {
+    return grpc::Status(grpc::StatusCode::NOT_FOUND, "Key not found");
+  }
+
+  response->set_value(*value);
+  return grpc::Status::OK;
 }
 
 grpc::Status KvStoreServiceImpl::Delete(grpc::ServerContext *,
